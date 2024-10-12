bcc429516639a5e066aeefd527b440487cdb93f7
touhidul_islam <t.sovon75@gmail.com>
2024-10-12 17:39:17 +0600

    october 12.2

---
+++ leetCode_168.py
@@ -0,0 +1,20 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        #alphabet_dict = {i: chr(64 + i) for i in range(1, 27)}
+        res = ""
+        while columnNumber > 0:
+            # adjusting the value if the number is divisible by 26
+            columnNumber -= 1
+
+            rem = columnNumber % 26
+            res += chr(65 + rem) # 'A' is ASCII 65
+            columnNumber //= 26
+
+        return res[::-1]
+
+
+
+
+sol = Solution()
+
+print(sol.convertToTitle(943566))
